def8932c4e4771aa6d9c475872d8e4fa5aca1053
Girimugan <girimugan@gmail.com>
2025-09-17 12:06:47 -0400

    update version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header.jsx";
 import Category from "./components/CategoryNav/Category.jsx";
 import BannerOne from "./components/PromotionalStrip/BannerOne.jsx";
 import BannerTwo from "./components/PromotionalStrip/BannerTwo.jsx";
 import CategoryCarousel from "./components/special stores/CategoryCarousel.jsx";
+import Routeone from "./components/routeing files/Routeone.jsx";
 
 import "./App.css";
 
@@ -18,7 +20,10 @@ function App() {
       <BannerOne />
       <BannerTwo />
       <CategoryCarousel />
+      <Routeone />
+      
     </div> 
+    
   );
 }
 
--- src/components/special stores/CategoryCarousel.jsx
+++ src/components/special stores/CategoryCarousel.jsx
@@ -4,6 +4,8 @@ import specialimg2 from "../../assets/specialimg2.jpg";
 import specialimg3 from "../../assets/specialimg3.jpg";
 import specialimg4 from "../../assets/specialimg4.jpg";
 import specialimg5 from "../../assets/specialimg5.jpg";
+import upiimg1 from "../../assets/upiimg1.jpg";
+import upiimg2 from "../../assets/upiimg2.jpg";
 import "./CategoryCarousel.css";
 
 function CategoryCarousel() {
@@ -15,7 +17,7 @@ function CategoryCarousel() {
         <img src={specialimg1} alt="first slider" />
       </div>
       <div className="specialbox2">
-        <img src={specialimg2} alt="sec slider" />
+        <img src={specialimg2} alt="second slider" />
       </div>
       <div className="specialbox3">
         <img src={specialimg3} alt="third slider" />
@@ -26,10 +28,18 @@ function CategoryCarousel() {
       <div className="specialbox5">
         <img src={specialimg5} alt="fifth slider" />
       </div>
+
       <div className="line1"></div>
+
+      <div className="upibox1">
+        <img src={upiimg1} alt="sixth slider" />
+      </div>
+      <div className="upibox2">
+        <img src={upiimg2} alt="seventh slider" />
+      </div>
     </div>
   );
 }
-
+``
 export default CategoryCarousel;
 
